7f83bc573954c9e3e7750596a86a1df45efed812
DeltaDev <votinovilya174@gmail.com>
2025-11-25 15:03:07 +0500

    Add files via upload

---
+++ plugins.lua
@@ -0,0 +1,56 @@
+-- Package manager for plugins
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+	{ 'phaazon/hop.nvim' },
+    
+    -- Neotree
+    {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v2.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim", "s1n7ax/nvim-window-picker"
+	}
+    },
+	
+    -- Syntax highlight
+	{ 'nvim-treesitter/nvim-treesitter', branch = 'master', lazy = false, build = ":TSUpdate"},
+	{ 'neovim/nvim-lspconfig' },
+	
+    -- Theme
+    { 'joshdick/onedark.vim' },
+    
+    -- atocomplits
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
+    { 'hrsh7th/nvim-cmp' },
+
+    -- Mason
+    { 
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate"
+    },
+
+    -- Telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        dependencies = {'nvim-lua/plenary.nvim'}
+    }
+})
