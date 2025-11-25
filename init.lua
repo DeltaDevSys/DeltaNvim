3b76451beeb21e0ec7ed17a6d2f0e6c026e73a1a
DeltaDev <votinovilya174@gmail.com>
2025-11-25 15:02:33 +0500

    Add files via upload
    Upload files

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- Basic
+require('core.plugins')
+require('core.mappings')
+require('core.colors')
+require('core.configs')
+
+-- Plugins
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.cmp')
+require('plugins.mason')
+require('plugins.telescope')
