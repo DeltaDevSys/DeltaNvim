24e1ec0f26b04d5f2add8016cf745614381a0219
DeltaDev <votinovilya174@gmail.com>
2025-12-09 21:35:42 +0500

    Update dashboard.lua

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,1 +1,34 @@
-require('dashboard').setup {}
+require('dashboard').setup {
+    theme = 'hyper',
+    config = {
+        header = {
+            '',
+            '██████╗ ███████╗██╗  ████████╗ █████╗ ',
+            '██╔══██╗██╔════╝██║  ╚══██╔══╝██╔══██╗',
+            '██║  ██║█████╗  ██║     ██║   ███████║',
+            '██║  ██║██╔══╝  ██║     ██║   ██╔══██║',
+            '██████╔╝███████╗███████╗██║   ██║  ██║',
+            '╚═════╝ ╚══════╝╚══════╝╚═╝   ╚═╝  ╚═╝',
+            '',
+        },
+        shortcut = {
+            { desc = ' New File', group = 'Label', action = 'enew', key = 'n' },
+            { desc = ' Files', group = 'Label', action = 'Telescope find_files', key = 'f' },
+            { desc = ' Quit', group = 'Label', action = 'qa', key = 'q' },
+        },
+        footer = {},
+    },
+
+    hide = {
+        statusline = true,       -- hide statusline default is true
+        tabline = true,         -- hide the tabline
+        winbar = true,          -- hide winbar
+    },
+
+    preview = {
+        command,          -- preview command
+        file_path,        -- preview file path
+        file_height,      -- preview file height
+        file_width,       -- preview file width
+    },
+}
