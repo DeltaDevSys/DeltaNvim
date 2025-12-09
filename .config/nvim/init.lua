118892099c1622581f6784ecddffe6b153617194
DeltaDev <votinovilya174@gmail.com>
2025-12-09 16:51:29 +0500

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,13 @@
--- Basic
+- настройка и писок плагинов для установки
 require('core.plugins')
 require('core.mappings')
-require('core.colors')
 require('core.configs')
 
--- Plugins
+-- плагины
 require('plugins.neotree')
 require('plugins.treesitter')
 require('plugins.cmp')
 require('plugins.mason')
 require('plugins.telescope')
+require('plugins.lualine')
+require('plugins.bufferline')
